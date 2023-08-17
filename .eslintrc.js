ee430233af82c417111de7004893feb4725e422b
Aleksei <aleksei@domain.local>
2023-08-17 14:57:19 +0300

    chore: Добавлены предпочтительные правила в eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,45 @@
+const commonRules = {
+  'import/prefer-default-export': 'off',
+  'import/no-default-export': 'error',
+  'import/imports-first': ['error', 'absolute-first'],
+  'no-param-reassign': [
+    'error',
+    { props: true, ignorePropertyModificationsFor: ['draft'] },
+  ],
+  'react/function-component-definition': [
+    2,
+    {
+      namedComponents: ['function-declaration', 'arrow-function'],
+    },
+  ],
+  'react/jsx-no-useless-fragment': [
+    'error',
+    {
+      allowExpressions: true,
+    },
+  ],
+  '@typescript-eslint/explicit-function-return-type': 'error',
+  'padding-line-between-statements': [
+    'error',
+    { blankLine: 'always', prev: '*', next: ['return', 'block-like'] },
+    { blankLine: 'always', prev: ['block-like'], next: '*' },
+  ],
+};
+
 module.exports = {
-  extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: { ...commonRules },
+  overrides: [
+    {
+      files: ['src/app/**/page.tsx', 'src/app/**/layout.tsx'],
+      rules: {
+        ...commonRules,
+        'import/no-default-export': 'off',
+      },
+    },
+  ],
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
+}): React.ReactElement {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
